3e859465a98b3659f3bb785f39fb3cccb763b4b5
Gardiner Nardini <gardiner.nardini@gmail.com>
2018-06-24 05:53:14 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -15,7 +15,9 @@ def first_challenge
   contacts.each do |person,data|
     data.each do |attribute,value|
       if attribute == :favorite_icecream_flavors
-        value.
+        value.delete_if do |flavor|
+          flavor == "strawberry"
+        end
       end
     end
   end
